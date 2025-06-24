1d603497ea6f246054dbfd2ec0025f17ba9203ff
hyun3138 <namhyun3418@gmail.com>
2025-06-24 14:50:22 +0900

    index

---
+++ my-app/src/index.tsx
@@ -0,0 +1,21 @@
+// index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} else {
+  console.error("⚠️ 'root' element not found in index.html");
+}
