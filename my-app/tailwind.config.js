24ab10ebd0e699ed6a41274c0a72b0f57b89be9a
hyun3138 <namhyun3418@gmail.com>
2025-06-25 10:39:50 +0900

    merge

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
           '0%': { opacity: 0, transform: 'translateY(16px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
         },
-      },n
+      },
     },
   },
   plugins: [],
